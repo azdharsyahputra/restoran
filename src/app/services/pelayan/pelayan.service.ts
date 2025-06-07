6b33fc4fadf4eb4f58c7d10bf7f33c037189f9b7
R421S <farrasoficial@gmail.com>
2025-06-07 11:43:11 +0700

    push backend semua halaman frontend yang ada

--- src/app/services/pelayan/pelayan.service.ts
+++ src/app/services/pelayan/pelayan.service.ts
@@ -1,4 +1,3 @@
-// src/app/services/pelayan/pelayan.service.ts
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -9,38 +8,60 @@ import { environment } from '../../../environments/environment';
 })
 export class PelayanService {
 
-  private apiUrl = `${environment.apiUrl}/pelayan/reservasi`;
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders(token: string): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json'
+    });
+  }
+
   getReservasi(token: string): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json'
-    });
-
-    return this.http.get(this.apiUrl, { headers });
+    // GET /pelayan/reservasi
+    const url = `${this.baseUrl}/pelayan/reservasi`;
+    return this.http.get(url, { headers: this.getHeaders(token) });
   }
 
-  // Tambahkan method untuk ambil detail reservasi berdasarkan ID
   getDetailReservasi(reservasiId: number, token: string): Observable<any> {
-    const url = `${this.apiUrl}/${reservasiId}`;
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json'
-    });
-
-    return this.http.get(url, { headers });
+    // GET /pelayan/reservasi/{id}
+    const url = `${this.baseUrl}/pelayan/reservasi/${reservasiId}`;
+    return this.http.get(url, { headers: this.getHeaders(token) });
   }
 
-  // Jika ingin post konfirmasi meja juga, bisa tambah method ini
   konfirmasiMeja(reservasiId: number, data: any, token: string): Observable<any> {
-    const url = `${this.apiUrl}/${reservasiId}/konfirmasi-meja`;
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/json'
-    });
-
-    return this.http.post(url, data, { headers });
+    // POST /pelayan/reservasi/{id}/konfirmasi-meja
+    const url = `${this.baseUrl}/pelayan/reservasi/${reservasiId}/konfirmasi-meja`;
+    return this.http.post(url, data, { headers: this.getHeaders(token) });
   }
+
+  getReservasiPelayan(token: string): Observable<any> {
+    // GET /pelayan/kehadiran-reservasi
+    const url = `${this.baseUrl}/pelayan/kehadiran-reservasi`;
+    return this.http.get<any>(url, { headers: this.getHeaders(token) });
+  }
+
+  konfirmasiReservasi(id: number, status: string, token: string): Observable<any> {
+    // PUT /pelayan/reservasi/{id}/konfirmasi
+    const url = `${this.baseUrl}/pelayan/reservasi/${id}/konfirmasi`;
+    return this.http.put<any>(url, { status }, { headers: this.getHeaders(token) });
+  }
+
+  // history buat di pelayan
+  getHistoryReservasi(token: string): Observable<any> {
+    const url = `${this.baseUrl}/pelayan/history-reservasi`;
+    return this.http.get<any>(url, {
+      headers: this.getHeaders(token)
+    });
+  }
+
+  // data meja di pelayan
+  getMeja(token: string): Observable<any> {
+    const url = `${this.baseUrl}/pelayan/meja`;
+    return this.http.get<any>(url, { headers: this.getHeaders(token) });
+  }
+
+
 }
--- src/app/status-meja/status-meja.page.ts
+++ src/app/status-meja/status-meja.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PelayanService } from '../services/pelayan/pelayan.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-status-meja',
@@ -8,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusMejaPage implements OnInit {
 
-  constructor() { }
+  mejaList: any[] = [];
+  filteredMejaList: any[] = [];
+  loading = false;
+  token: string = '';
+  filterStatus: string = ''; // kosong berarti semua
+
+  constructor(
+    private pelayanService: PelayanService,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {
+    this.token = localStorage.getItem('token') || '';
+    this.loadMeja();
   }
 
+  async loadMeja() {
+    this.loading = true;
+    try {
+      const response = await this.pelayanService.getMeja(this.token).toPromise();
+      this.mejaList = response.data || [];
+      this.applyFilter();
+    } catch (error) {
+      const alert = await this.alertCtrl.create({
+        header: 'Gagal',
+        message: 'Gagal memuat data meja.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  applyFilter() {
+    if (!this.filterStatus) {
+      this.filteredMejaList = this.mejaList;
+    } else {
+      this.filteredMejaList = this.mejaList.filter(m => m.status === this.filterStatus);
+    }
+  }
+
+  onStatusChange(event: any) {
+    this.filterStatus = event.detail.value;
+    this.applyFilter();
+  }
 }
