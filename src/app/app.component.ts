2454d617ace780fc36186d3653cc8f0961a92418
R421S <farrasoficial@gmail.com>
2025-06-13 16:45:08 +0700

    ganti url pake hostingan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +7,5 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private router: Router,
-  ) {
-    this.platform.ready().then(() => {
-      const token = localStorage.getItem('token');
-      const role = localStorage.getItem('role');
-
-      if (!token || !role) {
-        this.router.navigateByUrl('/login', { replaceUrl: true });
-      }
-      // Jangan langsung redirect ke halaman role,
-      // biarkan user bebas pindah ke halaman lain lewat UI
-    });
-  }
+  constructor() {}
 }
