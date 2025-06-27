cb924b3ff9bc7b5ace298669e8e96ebf047ddf97
R421S <farrasoficial@gmail.com>
2025-06-27 21:17:38 +0700

    ganti base url foto-menu ke public

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // imageBaseUrl: 'https://restoran.kalasenja.my.id/storage/gambar_menu/',
   // imageBaseUrlProfile: 'https://restoran.kalasenja.my.id/storage/profile/'
   apiUrl: 'http://localhost:8000/api',
-  imageBaseUrl: 'http://localhost:8000/storage/gambar_menu/',
+  imageBaseUrl: 'http://localhost:8000/img/gambar_menu/',
   imageBaseUrlProfile: 'http://localhost:8000/storage/profile/'
   
 };
